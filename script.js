9d4a3a7b43ae9e19db8faf9a08983bab7165a82f
wallanmota <wallanmota.13@gmail.com>
2021-09-15 18:53:26 -0300

    imprimindo valor na tela

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ document.querySelector('#contador').addEventListener('submit', (event)=>{
     let inicio = document.querySelector('#primeiro').value;
     let fim = document.querySelector('#ultimo').value;
     let passo = document.querySelector('#passo').value;
+    var r = document.querySelector('#result');
 
-    for(let i = Number(inicio); i <= Number(fim); i += Number(passo)){
-
-        console.log(i);
-        
-    }
+    if(inicio.length == 0 || fim.length == 0 || passo.length == 0){
+        alert("Digite todos os valores")
+    }else{
+        for(let i = Number(inicio); i <= Number(fim); i += Number(passo)){
+            r.innerHTML += ` ${i} `
+            
+        }
+    } 
 })
