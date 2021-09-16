94f3f94880ea3f91a3ebfbcf7b813fe24a267cd1
wallanmota <wallanmota.13@gmail.com>
2021-09-16 13:37:19 -0300

    logica finalizada

--- script.js
+++ script.js
@@ -5,13 +5,31 @@ document.querySelector('#contador').addEventListener('submit', (event)=>{
     let fim = document.querySelector('#ultimo').value;
     let passo = document.querySelector('#passo').value;
     var r = document.querySelector('#result');
+    clear();
+
+    
 
     if(inicio.length == 0 || fim.length == 0 || passo.length == 0){
         alert("Digite todos os valores")
+    }else if(Number(fim) < Number(inicio)){
+        for(let i = Number(inicio); i >= Number(fim); i -= Number(passo)){
+            validaPasso();
+            r.innerHTML += ` ${i} &#x1F449;`
+        }r.innerHTML += ' &#x1F3C1'
     }else{
         for(let i = Number(inicio); i <= Number(fim); i += Number(passo)){
-            r.innerHTML += ` ${i} `
-            
-        }
+            validaPasso();
+            r.innerHTML += ` ${i} &#x1F449;`    
+        }r.innerHTML += ' &#x1F3C1'
     } 
+
+    function validaPasso(){
+        if(Number(passo) <= 0){
+            alert("Passo não pode ser 0 será considerado o passo 1")
+            passo = 1;
+        }
+    }
+    function clear(){
+        r.innerHTML = "";
+    }
 })
